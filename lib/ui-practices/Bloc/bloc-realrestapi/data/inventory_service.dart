7f23ef11c978ba93b9e0841aa111813c9fc35e3f
Vishal Sawant <sawantvishal2001@gmail.com>
2026-09-18 21:07:33 +0530

    bloc with product rest

---
+++ lib/ui-practices/Bloc/bloc-realrestapi/data/inventory_service.dart
@@ -0,0 +1,7 @@
+class InventoryService {
+  final String baseUrl = "https://inventory-management-springboot.onrender.com";
+
+  Future<List<Map<String, dynamic>>> fetchInventory() async {
+    final response = await http.
+  }
+}
